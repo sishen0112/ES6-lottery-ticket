5a8af837ae0276c014b3cad871981dc44e52aebf
Plover Zhang <mz_zhangpeng@outlook.com>
2017-05-29 10:10:36 +0800

    创建js编译任务脚本

--- tasks/util/args.js
+++ tasks/util/args.js
@@ -1,1 +1,33 @@
 // util/ 放置常见脚本
+
+// 引入处理命令行参数的包
+import yargs from 'yargs'
+
+// 区分开发环境和线上环境
+const args = yargs
+  .option('production', {   // 命令行选项部分
+    boolean: true,
+    default: false,
+    describe: 'min all scripts'
+  })
+  .option('watch', {
+    boolean: true,
+    default: false,
+    describe: 'watch all files'
+  })
+  .option('verbose', {
+    boolean: true,
+    default: false,
+    describe: 'log'
+  })
+  .option('sourcemaps', {
+    describe: 'force the creation of sourcemaps'
+  })
+
+  .option('port', {
+    string: true,
+    default: 8080,
+    describe: 'server port'
+  })
+
+  .argv
